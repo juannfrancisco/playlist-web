174b83428258c0ecbd92fb59796ca2ebd62473d6
Juan Francisco <juan.maldonado.leon@gmail.com>
2017-11-14 13:24:42 -0300

    actualizacion de playlist

--- src/main/java/cl/accenture/curso_java/playlist/controladores/VerPlaylistControlador.java
+++ src/main/java/cl/accenture/curso_java/playlist/controladores/VerPlaylistControlador.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 import cl.accenture.curso_java.playlist.dao.PlaylistDAO;
 import cl.accenture.curso_java.playlist.modelo.Cancion;
@@ -18,7 +18,7 @@ import cl.accenture.curso_java.playlist.modelo.Playlist;
  *
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class VerPlaylistControlador {
 
 	private Playlist playlist;
@@ -52,6 +52,37 @@ public class VerPlaylistControlador {
 			@Override
 			public int compare(Cancion arg0, Cancion arg1) {
 				return arg0.getNombre().toLowerCase().compareTo( arg1.getNombre().toLowerCase() );
+			}
+		} );
+	}
+	
+	public void ordenarPorGenero(){
+		Collections.sort( this.playlist.getCanciones(), new Comparator<Cancion>() {
+			@Override
+			public int compare(Cancion arg0, Cancion arg1) {
+				return arg0.getGenero().toLowerCase().compareTo( arg1.getGenero().toLowerCase() );
+			}
+		} );
+	}
+	
+	public void ordenarPorArtista(){
+		Collections.sort( this.playlist.getCanciones(), new Comparator<Cancion>() {
+			@Override
+			public int compare(Cancion arg0, Cancion arg1) {
+				return arg0.getArtista().toLowerCase().compareTo( arg1.getArtista().toLowerCase() );
+			}
+		} );
+	}
+	
+	public void ordenarPorDuracion(){
+		Collections.sort( this.playlist.getCanciones(), new Comparator<Cancion>() {
+			@Override
+			public int compare(Cancion arg0, Cancion arg1) {
+				if( arg0.getDuracion() > arg1.getDuracion() )
+					return 1;
+				if( arg0.getDuracion() < arg1.getDuracion() )
+					return -1;
+				return 0;
 			}
 		} );
 	}
